6d0545d7efb7d3b9e8710df69abc6d913706e5b3
mani sudheer <manisudheer383@gmail.com>
2026-06-27 11:47:56 +0530

    fix

--- src/app/api/donations/route.ts
+++ src/app/api/donations/route.ts
@@ -1,28 +1,5 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/src/app/lib/prisma";
-import { Prisma } from "@prisma/client";
-
-type DonorWithDonations = Prisma.DonorGetPayload<{
-  select: {
-    id: true;
-    fullName: true;
-    email: true;
-    mobile: true;
-    donorType: true;
-    donations: {
-      select: {
-        id: true;
-        amount: true;
-        title: true;
-        description: true;
-        paymentStatus: true;
-        razorpayOrderId: true;
-        razorpayPaymentId: true;
-        createdAt: true;
-      };
-    };
-  };
-}>;
 
 export async function GET() {
   try {
@@ -51,20 +28,20 @@ export async function GET() {
       },
     });
 
-   const data = donors.map((donor) => {
-  const totalDonations = donor.donations.length;
+    const data = donors.map((donor) => {
+      const totalDonations = donor.donations.length;
 
-  const totalAmount = donor.donations.reduce(
-    (sum, d) => sum + d.amount,
-    0
-  );
+      const totalAmount = donor.donations.reduce(
+        (sum, d) => sum + d.amount,
+        0
+      );
 
-  return {
-    ...donor,
-    totalDonations,
-    totalAmount,
-  };
-});
+      return {
+        ...donor,
+        totalDonations,
+        totalAmount,
+      };
+    });
 
     return NextResponse.json({
       success: true,
